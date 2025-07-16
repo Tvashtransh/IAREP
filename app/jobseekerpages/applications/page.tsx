8262a88f8ef1e8440fc6b1becd794a8dc29fd631
Tvashtaransh <Priyanshugdscneo@gmail.com>
2025-07-16 13:55:40 +0530

    Remove jobseekerpages directory from git tracking

--- app/jobseekerpages/applications/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ApplicationsPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">My Applications</h1>
-      {/* Application tracking components will go here */}
-    </div>
-  );
-} 
